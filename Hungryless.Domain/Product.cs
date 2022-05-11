eff6d69420ab967d5c569dde2cc271dcaf44022f
Phnc <paulohn27@hotmail.com>
2022-05-11 10:28:56 -0300

    Adicionar arquivos de projeto.

---
+++ Hungryless.Domain/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hungryless.Domain
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+    }
+}
