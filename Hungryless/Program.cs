5d514d804991257aeaa0158750659e2c28753761
Phnc <paulohn27@hotmail.com>
2022-05-11 10:44:36 -0300

    adjust Program.cs and install dependencies

--- Hungryless/Program.cs
+++ Hungryless/Program.cs
@@ -1,7 +1,10 @@
+using Hungryless.Infra;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ProductContext>();
 
 var app = builder.Build();
 
